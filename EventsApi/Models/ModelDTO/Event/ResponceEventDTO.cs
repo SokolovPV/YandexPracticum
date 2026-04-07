a76ebc9186acc5e04abc9ee626b4d4f03c5bcc2e
Павел Соколов <pavel44187@hotmail.com>
2026-04-07 20:50:10 +0300

    переименовал модель DTO

--- EventsApi/Models/ModelDTO/Event/ResponceEventDTO.cs
+++ EventsApi/Models/ModelDTO/Event/ResponceEventDTO.cs
@@ -8,5 +8,5 @@
   /// <param name="Description">Описание</param>
   /// <param name="StartAt">Дата начала</param>
   /// <param name="EndAt">Дата окончания</param>
-  public record EventDTO(Guid Id, string Title, string? Description, DateTime StartAt, DateTime EndAt);
+  public record ResponceEventDTO(Guid Id, string Title, string? Description, DateTime StartAt, DateTime EndAt);
 }
