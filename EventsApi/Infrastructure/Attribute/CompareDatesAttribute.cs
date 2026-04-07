00e5986832c16d56be4404919755c34ce898999e
Павел Соколов <pavel44187@hotmail.com>
2026-04-07 23:03:43 +0300

    Переписал атрибут сравнения дат

---
+++ EventsApi/Infrastructure/Attribute/CompareDatesAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventsApi.Infrastructure.Attribute
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CompareDatesAttribute : ValidationAttribute
+    {
+        public string PropertyName { get; set; }
+
+        public CompareDatesAttribute(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var propertyInfo = validationContext.ObjectType.GetProperty(PropertyName);
+            if (propertyInfo == null)
+                return new ValidationResult($"Свойство {PropertyName} не найдено.");
+
+            var startDateValue = (DateTime)propertyInfo.GetValue(validationContext.ObjectInstance);
+            var endDateValue = (DateTime)value;
+
+            if (endDateValue <= startDateValue)
+                return new ValidationResult("Дата окончания должна быть больше даты начала.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
