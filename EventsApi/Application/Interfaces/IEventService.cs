9762a4803027ae15fccda6ee66ef15237fe482d7
Павел Соколов <pavel44187@hotmail.com>
2026-04-07 20:54:14 +0300

    переименовал модель DTO

--- EventsApi/Application/Interfaces/IEventService.cs
+++ EventsApi/Application/Interfaces/IEventService.cs
@@ -2,12 +2,12 @@
 
 namespace EventsApi.Application.Interfaces
 {
-	public interface IEventService
-	{
-		PaginatedResult GetEvents(string? title, DateTime? from, DateTime? to, int? page, int? pageSize);
-		EventDTO? GetEvent(Guid id);
-		EventDTO AddEvent(InputEventDTO createEventDTO);
-		bool ChangeEvent(Guid id, InputEventDTO updateEvent);
-		bool RemoveEvent(Guid id);
-	}
+    public interface IEventService
+    {
+        PaginatedResult GetEvents(string? title, DateTime? from, DateTime? to, int? page, int? pageSize);
+        ResponceEventDTO? GetEvent(Guid id);
+        ResponceEventDTO AddEvent(InputEventDTO createEventDTO);
+        bool ChangeEvent(Guid id, InputEventDTO updateEvent);
+        bool RemoveEvent(Guid id);
+    }
 }
