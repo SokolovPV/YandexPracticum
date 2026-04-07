05c30e6dfb7df0fe3b85d1ea2863dca0f0eb3f4a
Павел Соколов <pavel44187@hotmail.com>
2026-04-07 23:40:31 +0300

    добавил интерфейс репозитория IEventRepository

---
+++ EventsApi/Infrastructure/Interfaces/IEventRepository.cs
@@ -0,0 +1,53 @@
+﻿using EventsApi.Models.Domain;
+using System.Linq.Expressions;
+
+namespace EventsApi.Infrastructure.Interfaces
+{
+    public interface IEventRepository
+    {
+        /// <summary>
+        /// Добавление события в репозиторий
+        /// </summary>
+        /// <param name="event">Само событие</param>
+        /// <param name="ct">Токен отмены</param>
+        Task AddAsync(Event @event, CancellationToken ct);
+
+        /// <summary>
+        /// Удаление события из репозитория
+        /// </summary>
+        /// <param name="id">ID события</param>
+        /// <param name="ct">Токен отмены</param>
+        Task<bool> DeleteAsync(Guid id, CancellationToken ct);
+
+        /// <summary>
+        /// Получение события по ID
+        /// </summary>
+        /// <param name="id">ID события</param>
+        /// <param name="ct">Токен отмены</param>
+        Task<Event?> GetByIdAsync(Guid id, CancellationToken ct);
+
+        /// <summary>
+        /// Обновление события
+        /// </summary>
+        /// <param name="event">Само событие</param>
+        /// <param name="ct">Токен отмены</param>
+        Task UpdateAsync(Event @event, CancellationToken ct);
+
+
+        /// <summary>
+        /// Получение событий с фильтрацией и пагинацией
+        /// </summary>
+        /// <param name="query">Предикат для фильтрации событий</param>
+        /// <param name="page">Номер страницы с данными для возврата</param>
+        /// <param name="pageSize">Количество элементов на странице</param>
+        /// <param name="ct">Токен отмены</param>
+        List<Event> ListAsync(Expression<Func<Event, bool>> query, int page, int pageSize, CancellationToken ct);
+
+        /// <summary>
+        /// Получение количества элементов в базе по фильтру
+        /// </summary>
+        /// <param name="query">Предикат для фильтрации событий</param>
+        /// <param name="ct">Токен отмены</param>
+        Task<int> CountAsync(Expression<Func<Event, bool>> query, CancellationToken ct);
+    }
+}
