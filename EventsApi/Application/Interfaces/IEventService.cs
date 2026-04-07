f85527c184cf87efd774c1188942fb71d954ad1e
Павел Соколов <pavel44187@hotmail.com>
2026-04-07 23:30:59 +0300

    асинхронный интерфейс IEventService

--- EventsApi/Application/Interfaces/IEventService.cs
+++ EventsApi/Application/Interfaces/IEventService.cs
@@ -2,12 +2,50 @@
 
 namespace EventsApi.Application.Interfaces
 {
+    /// <summary>
+    /// Интерфейс сервиса событий
+    /// </summary>
     public interface IEventService
     {
-        PaginatedResult GetEvents(string? title, DateTime? from, DateTime? to, int? page, int? pageSize);
-        ResponceEventDTO? GetEvent(Guid id);
-        ResponceEventDTO AddEvent(InputEventDTO createEventDTO);
-        bool ChangeEvent(Guid id, InputEventDTO updateEvent);
-        bool RemoveEvent(Guid id);
+        /// <summary>
+        /// Создание события
+        /// </summary>
+        /// <param name="createEventDTO">Входящая модель создания события</param>
+        /// <param name="ct">токен отмены</param>
+        /// <returns></returns>
+        Task<ResponceEventDTO> AddEvent(InputEventDTO createEventDTO, CancellationToken ct);
+
+
+        /// <summary>
+        /// Получение событий с пагинацией
+        /// </summary>
+        /// <param name="filter">Фильтр для пагинации</param>
+        /// <param name="ct">Токен отмены</param>
+        PaginatedResult GetEvents(EventsFilter filter, CancellationToken ct);
+
+
+        /// <summary>
+        /// Поиск события
+        /// </summary>
+        /// <param name="eventId">ID события</param>
+        /// <param name="ct">Токен отмены</param>
+        Task<ResponceEventDTO?> GetEvent(Guid eventId, CancellationToken ct);
+
+        /// <summary>
+        /// Обновление события
+        /// </summary>
+        /// <param name="eventId">ID события</param>
+        /// <param name="updateEvent">Входящая модель обновления события </param>
+        /// <param name="ct">Токен отмены</param>
+        /// <returns></returns>
+        Task<bool> ChangeEvent(Guid eventId, InputEventDTO updateEvent, CancellationToken ct);
+
+
+        /// <summary>
+        /// Удаление события
+        /// </summary>
+        /// <param name="eventId">ID события</param>
+        /// <param name="ct">Токен отмены</param>
+        Task<bool> RemoveEvent(Guid eventId, CancellationToken ct);
     }
 }
