a8759a154bb22f0cd481d2e054ffde03e8ddc467
Павел Соколов <pavel44187@gmail.com>
2026-04-26 19:41:20 +0300

    вынес константы в отдельный файл

---
+++ EventsApi/Application/Services/ConstantValues.cs
@@ -0,0 +1,8 @@
+namespace EventsApi.Application.Services;
+
+public static class ConstantValues
+{
+   public const string key_not_found_exception = "Идентификатор мероприятия не найден.";
+   public const string dateFrom_more_dateTo_exception = "Дата начала мероприятия больше даты завершения.";
+   public const string totalSeats_more_range_exception = "Общее количество мест на событие должно быть больше 1 и меньше 100.";
+}
