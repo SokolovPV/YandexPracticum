a479baca2bc6fe425e7e2cacdbcbdc9a38617747
Павел Соколов <pavel44187@gmail.com>
2026-04-26 22:13:22 +0300

    константы из общего статического класса

--- EventsApi/Application/Services/BookingService.cs
+++ EventsApi/Application/Services/BookingService.cs
@@ -9,7 +9,6 @@ namespace EventsApi.Application.Services;
 public class BookingService(IEventRepository eventRepository, IBookingRepository bookingRepository, ILogger<BookingService> logger) : IBookingService
 {
     private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-    private const string key_not_found_exception = "Идентификатор бронирования не найден.";
     /// <inheritdoc/>
     public async Task<Booking> CreateBookingAsync(Guid eventId, CancellationToken ct)
     {
@@ -22,7 +21,7 @@ public class BookingService(IEventRepository eventRepository, IBookingRepository
             if (_event is null)
             {
                 logger.LogError("Идентификатор мероприятия {Id} не найден.", eventId);
-                throw new KeyNotExistException(eventId, key_not_found_exception);
+                throw new KeyNotExistException(eventId, ConstantValues.key_not_found_exception);
             }
             if (!_event.TryReserveSeats())
                 throw new NoAvailableSeatsException($"Для события ID={_event.Id} отстутствуют свободные места для бронирования");
@@ -45,7 +44,7 @@ public class BookingService(IEventRepository eventRepository, IBookingRepository
         logger.LogInformation("Получение бронирования : {bookingId}", bookingId);
         var booking = await bookingRepository.GetByIdAsync(bookingId, ct);
         if (booking is null)
-            throw new KeyNotExistException(bookingId, key_not_found_exception);
+            throw new KeyNotExistException(bookingId, ConstantValues.key_not_found_exception);
         return booking;
     }
 }
