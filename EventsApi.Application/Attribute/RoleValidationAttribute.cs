eedb70c510cd534909a881d2ced2187d52a585ea
Павел Соколов <pavel44187@hotmail.com>
2026-06-23 21:30:46 +0300

    добавил атрибут RoleValidationAttribute для модели DTO

--- EventsApi.Application/Attribute/RoleValidationAttribute.cs
+++ EventsApi.Application/Attribute/RoleValidationAttribute.cs
@@ -5,24 +5,16 @@ namespace EventsApi.Application.Attribute;
 [AttributeUsage(AttributeTargets.Property)]
 public class RoleValidationAttribute : ValidationAttribute
 {
-    private static readonly HashSet<string> roles =
-              new HashSet<string>(Enum.GetNames(typeof(RoleType)).Select(name => name.ToLowerInvariant())
-
-    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        var propertyInfo = validationContext.ObjectType.GetProperty(PropertyName);
-        if (propertyInfo == null)
-            return new ValidationResult($"Свойство {PropertyName} не найдено.");
-
-        var startDateValue = (DateTime?)propertyInfo.GetValue(validationContext.ObjectInstance);
-        var endDateValue = (DateTime?)value;
-        // Если endDateValue == null, то пропускаем валидацию
-        if (!endDateValue.HasValue)
-            return ValidationResult.Success!;
-
-        if (startDateValue.HasValue && endDateValue.HasValue && endDateValue <= startDateValue)
-            return new ValidationResult("Дата окончания должна быть больше даты начала.");
-
-        return ValidationResult.Success!;
+        if (value == null)
+            return new ValidationResult("Роль не указана.");
+        
+        if (!Enum.IsDefined(typeof(RoleType), value))
+        {
+            return new ValidationResult($"Недопустимое значение роли. Допустимые значения: {string.Join(", ", Enum.GetNames(typeof(RoleType)))}");
+        }
+        
+        return ValidationResult.Success;
     }
 }
