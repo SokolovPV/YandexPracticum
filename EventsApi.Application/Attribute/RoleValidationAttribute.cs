9614dfa57b4712a9ee2ada4649e7d6b203f0bf43
Pavel Sokolov <pavel44187@gmail.com>
2026-06-24 17:46:36 +0300

    Атрибут валидации роли

--- EventsApi.Application/Attribute/RoleValidationAttribute.cs
+++ EventsApi.Application/Attribute/RoleValidationAttribute.cs
@@ -6,23 +6,17 @@ namespace EventsApi.Application.Attribute;
 public class RoleValidationAttribute : ValidationAttribute
 {
     private static readonly HashSet<string> roles =
-              new HashSet<string>(Enum.GetNames(typeof(RoleType)).Select(name => name.ToLowerInvariant())
+              new HashSet<string>(Enum.GetNames(typeof(RoleType)).Select(name => name.ToLowerInvariant()));
 
-    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        var propertyInfo = validationContext.ObjectType.GetProperty(PropertyName);
-        if (propertyInfo == null)
-            return new ValidationResult($"Свойство {PropertyName} не найдено.");
+        if (value == null)
+            return new ValidationResult($"Роль не задана");
 
-        var startDateValue = (DateTime?)propertyInfo.GetValue(validationContext.ObjectInstance);
-        var endDateValue = (DateTime?)value;
-        // Если endDateValue == null, то пропускаем валидацию
-        if (!endDateValue.HasValue)
-            return ValidationResult.Success!;
-
-        if (startDateValue.HasValue && endDateValue.HasValue && endDateValue <= startDateValue)
-            return new ValidationResult("Дата окончания должна быть больше даты начала.");
-
-        return ValidationResult.Success!;
+        if (!roles.Contains(value.ToString()))
+        {
+            return new ValidationResult($"Недопустимая роль. Допустимые значения: {string.Join(", ", roles)}");
+        }
+        return ValidationResult.Success;
     }
 }
