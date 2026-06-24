c6b103b8e9235fd4f4e7d5631fb7c07e5a728df5
Pavel Sokolov <pavel44187@gmail.com>
2026-06-24 17:53:35 +0300

    "пользователь не имеет прав на выполнение действия"

---
+++ EventsApi.Domain/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,27 @@
+namespace EventsApi.Domain.Exceptions;
+
+/// <summary>
+/// Исключение, для случая "пользователь не имеет прав на выполнение действия"
+/// </summary>
+public sealed class AccessDeniedException : Exception
+{
+    // Дополнительные свойства
+    public string? UserId { get; }
+    public string? RequiredRole { get; }
+    public string? ActionName { get; }
+
+
+    // Базовые конструкторы
+    public AccessDeniedException() 
+        : base("Доступ запрещен. У пользователя недостаточно прав.") { }
+
+    public AccessDeniedException(string message) : base(message) { }
+
+
+    public AccessDeniedException(string userId, string action)
+        : base($"Пользователь '{userId}' не имеет прав на выполнение действия '{action}'")
+    {
+        UserId = userId;
+        ActionName = action;
+    }
+}
