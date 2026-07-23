fd8cff4add042ef17bc38238bba941d381444dc0
Pavel Sokolov <pavel44187@gmail.com>
2026-07-23 11:45:13 +0300

    инвалидация кеша в метод сервиса

--- EventFlow.Events/EventFlow.Events.Infrastructure/Services/BookingConfirmedConsumer.cs
+++ EventFlow.Events/EventFlow.Events.Infrastructure/Services/BookingConfirmedConsumer.cs
@@ -2,7 +2,6 @@
 using System.Text.Json;
 using Confluent.Kafka;
 using EventFlow.Entities.Brokers;
-using EventFlow.Entities.Redis;
 using EventFlow.Events.Application.Interfaces;
 using EventFlow.Events.Domain.Exceptions;
 using EventFlow.Events.Infrastructure.Options;
@@ -15,7 +14,6 @@ namespace EventFlow.Events.Infrastructure.Services;
 
 public class BookingConfirmedConsumer(
     IServiceScopeFactory scopeFactory,
-    ICacheService cache,
     IOptions<KafkaOptions> kafkaOptions,
     ILogger<BookingConfirmedConsumer> logger) : BackgroundService
 {
